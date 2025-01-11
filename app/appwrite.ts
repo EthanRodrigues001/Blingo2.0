404d02d32e45d51822fc7bb145258c9c4de69122
EthanRodrigues001 <ethanrodrigues25@gmail.com>
2025-01-11 23:50:33 +0530

    dns issues auth redirect fix 2

--- app/appwrite.ts
+++ app/appwrite.ts
@@ -1,7 +1,5 @@
-"use server";
-
 import { Client, Account, OAuthProvider } from "appwrite";
-import { headers } from "next/headers";
+
 export const client = new Client();
 
 client
@@ -10,19 +8,12 @@ client
 // .set(process.env.NEXT_PUBLIC_API_KEY);
 console.log(client);
 export const account = new Account(client);
-
-const headersData = await headers();
-const origin = headersData.get("origin");
-if (!origin) {
-  throw new Error("Origin header is missing");
-}
-
 export { ID } from "appwrite";
 
 // Go to OAuth provider login page
 account.createOAuth2Session(
   OAuthProvider.Github, // provider
-  `${origin}/`, // redirect here on success
-  `${origin}/login`, // redirect here on failure
+  "https://www.blingo.tech/", // redirect here on success
+  "https://www.blingo.tech/login/", // redirect here on failure
   ["repo", "user"] // scopes (optional)
 );
